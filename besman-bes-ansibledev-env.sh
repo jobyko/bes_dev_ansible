c22dfb130c59fb61e8e11dbeb0f7786ea3740dce
Joby K <79845600+jobyko@users.noreply.github.com>
2021-05-19 14:16:49 +0530

    updated

--- besman-bes-ansibledev-env.sh
+++ besman-bes-ansibledev-env.sh
@@ -2,7 +2,7 @@
 
 function __besman_install_bes-ansibledev-env {
 
-
+set -x
 echo "installing bes_dev_ansible environment"
 git clone https://github.com/jobyko/bes_dev_ansible.git
 cd bes_dev_ansible && ansible-playbook -v -K besman-bes_dev.yml
